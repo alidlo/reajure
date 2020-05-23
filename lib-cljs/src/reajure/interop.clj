4f7c25536368d961e50a082fa1dbf6acfa22e2a0
Alid <alidcastano@gmail.com>
2020-05-23 10:07:41 -0500

    fix conversion of second style keyword part to uppercase

--- lib-cljs/src/reajure/interop.clj
+++ lib-cljs/src/reajure/interop.clj
@@ -1,5 +1,6 @@
 (ns reajure.interop
-  (:require [helix.impl.props :as hxp]))
+  (:require [clojure.string :as str]
+            [helix.impl.props :as hxp]))
 
 ;; Intern all components into namespace.
 ;; Note: the desired `tags` must be statically declared since `c` object is not available in clj.
@@ -13,7 +14,9 @@
   [props style-kw]
   (if (get props style-kw)
     (assoc props style-kw (hxp/into-js-array (for [v (get props style-kw)]
-                                               (cond (keyword? v) (hxp/camel-case v)
+                                               (cond (keyword? v) (let [k (name v)
+                                                                        [k1 k2] (str/split k #"-")]
+                                                                    (str/join "" [k1 (str/upper-case k2)]))
                                                      (map? v)     (hxp/primitive-obj v)
                                                      :else v))))
     props))
