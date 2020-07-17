a87296cf0bd818ee9b702306254c65bd274e05f2
Alid <alidcastano@gmail.com>
2020-07-16 21:43:55 -0500

    fix parsing of keyword style props

--- reajure-cljs/src/reajure/interop.clj
+++ reajure-cljs/src/reajure/interop.clj
@@ -8,30 +8,30 @@
             lbl
             btn])
 
-(defn ->js-style 
+(defn parse-kw-style-props 
   "Convert cljs style keyword shorthands to their string equivalents.
    e.g. :flx-1 -> 'flx1' || :jc-sb -> 'jcSB'."
   [props sk]
   (if (vector? (get props sk))
     (assoc props sk
-           (into []
-                 (for [v (get props sk)]
+           (mapv (fn [v]
                    (if (keyword? v) (let [k (name v)
                                           [k1 k2] (str/split k #"-")]
                                       (if-not k2 k1 (str/join "" [k1 (str/upper-case k2)])))
-                       v))))
+                       v))
+                 (get props sk)))
     props))
 
 (comment
   ;; object style 
-  (->js-style {:style {:flex 1}} :style)
+  (parse-kw-style-props {:style {:flex 1}} :style)
   ;; vector style, with keyword shorthand
-  (->js-style {:style [:flx-1]} :style))
+  (parse-kw-style-props {:style [:flx-1]} :style))
 
-(defn ->js-props 
+(defn parse-style-props
   "Converts cljs `props` to js props."
   [props]
   (-> props
-      (->js-style :style)
-      (->js-style :textStyle)))
+      (parse-kw-style-props :style)
+      (parse-kw-style-props :textStyle)))
 
